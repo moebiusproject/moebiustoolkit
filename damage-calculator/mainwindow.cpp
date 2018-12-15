75eb3b693e21e1c91072354161ea7aeb1ce6c3e4
Alejandro Exojo <suy@badopi.org>
2018-12-15 14:13:37 +0100

    Move lambda to a named function
    Is always nice to save an indent level and to navigate better.

--- damage-calculator/mainwindow.cpp
+++ damage-calculator/mainwindow.cpp
@@ -6,6 +6,8 @@
 #include <QtWidgets>
 #include <QtCharts>
 
+#include <functional>
+
 using namespace QtCharts;
 
 struct MainWindow::Private
@@ -15,6 +17,8 @@ struct MainWindow::Private
     QChartView* chartView = nullptr;
 
     QVector<Ui::configuration> configurations;
+
+    void newPage();
 };
 
 MainWindow::MainWindow(QWidget* parent)
@@ -35,54 +39,57 @@ MainWindow::MainWindow(QWidget* parent)
 
     auto newButton = new QPushButton(tr("New"));
     d->tabs->setCornerWidget(newButton);
-    connect(newButton, &QPushButton::clicked, [this](){
-        auto widget = new QWidget;
-        d->configurations.append(Ui::configuration());
-        auto configuration = d->configurations.last();
-        configuration.setupUi(widget);
-        d->tabs->addTab(widget, tr("Configuration %1").arg(d->tabs->count()));
-        d->tabs->setCurrentWidget(widget);
-
-
-        auto setup = [](QSpinBox* proficiency, QSpinBox* dice, QSpinBox* sides,
-                QSpinBox* bonus, QLabel* result)
-        {
-            auto calculateStats = [=](){
-                const double min = proficiency->value() + bonus->value()
-                                 + dice->value();
-                const double max = proficiency->value() + bonus->value()
-                                 + dice->value() * sides->value();
-                const double avg = proficiency->value() + bonus->value()
-                                 + dice->value() * (0.5 + sides->value()/2.0);
-
-                result->setText(tr("Min/Max/Average: %1/%2/%3")
-                                .arg(int(min)).arg(int(max))
-                                .arg(avg, 0, 'f', 2));
-            };
-
-            connect(proficiency, qOverload<int>(&QSpinBox::valueChanged), calculateStats);
-            connect(dice,        qOverload<int>(&QSpinBox::valueChanged), calculateStats);
-            connect(sides,       qOverload<int>(&QSpinBox::valueChanged), calculateStats);
-            connect(bonus,       qOverload<int>(&QSpinBox::valueChanged), calculateStats);
-            calculateStats();
-        };
-
-        setup(configuration.proficiencyDamageModifier1,
-              configuration.weaponDamageDiceNumber1,
-              configuration.weaponDamageDiceSide1,
-              configuration.weaponDamageDiceBonus1,
-              configuration.damageDetail1);
-
-        setup(configuration.proficiencyDamageModifier2,
-              configuration.weaponDamageDiceNumber2,
-              configuration.weaponDamageDiceSide2,
-              configuration.weaponDamageDiceBonus2,
-              configuration.damageDetail2);
-    });
+    connect(newButton, &QPushButton::clicked,
+            std::bind(&MainWindow::Private::newPage, d));
 }
 
 MainWindow::~MainWindow()
 {
     delete d;
     d = nullptr;
+}
+
+void MainWindow::Private::newPage()
+{
+    auto widget = new QWidget;
+    configurations.append(Ui::configuration());
+    auto configuration = configurations.last();
+    configuration.setupUi(widget);
+    tabs->addTab(widget, tr("Configuration %1").arg(tabs->count()));
+    tabs->setCurrentWidget(widget);
+
+    auto setupStatsLabel = [](QSpinBox* proficiency, QSpinBox* dice, QSpinBox* sides,
+                              QSpinBox* bonus, QLabel* result)
+    {
+        auto calculateStats = [=](){
+            const double min = proficiency->value() + bonus->value()
+                             + dice->value();
+            const double max = proficiency->value() + bonus->value()
+                             + dice->value() * sides->value();
+            const double avg = proficiency->value() + bonus->value()
+                             + dice->value() * (0.5 + sides->value()/2.0);
+
+            result->setText(tr("Minimum/Maximum/Average: %1/%2/%3")
+                            .arg(int(min)).arg(int(max))
+                            .arg(avg, 0, 'f', 2));
+        };
+
+        connect(proficiency, qOverload<int>(&QSpinBox::valueChanged), calculateStats);
+        connect(dice,        qOverload<int>(&QSpinBox::valueChanged), calculateStats);
+        connect(sides,       qOverload<int>(&QSpinBox::valueChanged), calculateStats);
+        connect(bonus,       qOverload<int>(&QSpinBox::valueChanged), calculateStats);
+        calculateStats();
+    };
+
+    setupStatsLabel(configuration.proficiencyDamageModifier1,
+                    configuration.weaponDamageDiceNumber1,
+                    configuration.weaponDamageDiceSide1,
+                    configuration.weaponDamageDiceBonus1,
+                    configuration.damageDetail1);
+
+    setupStatsLabel(configuration.proficiencyDamageModifier2,
+                    configuration.weaponDamageDiceNumber2,
+                    configuration.weaponDamageDiceSide2,
+                    configuration.weaponDamageDiceBonus2,
+                    configuration.damageDetail2);
 }
