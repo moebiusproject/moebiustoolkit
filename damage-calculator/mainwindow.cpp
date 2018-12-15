de0c6c495a6a0c25260ef4e67eae7806ad85e27e
Alejandro Exojo <suy@badopi.org>
2018-12-15 12:04:26 +0100

    Show (empty) chart tab, and add configurations

--- damage-calculator/mainwindow.cpp
+++ damage-calculator/mainwindow.cpp
@@ -1,22 +1,47 @@
 #include "mainwindow.h"
 
+#include "ui_configuration.h"
+
 #include <QtCore>
 #include <QtWidgets>
+#include <QtCharts>
+
+using namespace QtCharts;
 
 struct MainWindow::Private
 {
-    QTabWidget* tabs;
+    QTabWidget* tabs = nullptr;
+    QChart* chart = nullptr;
+    QChartView* chartView = nullptr;
+
+    QVector<Ui::configuration> configurations;
 };
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , d(new MainWindow::Private)
 {
+    d->chart = new QChart;
+    d->chartView = new QChartView(d->chart);
+    d->chartView->setRenderHint(QPainter::Antialiasing);
+
     d->tabs = new QTabWidget(this);
     setCentralWidget(d->tabs);
+
+    // TODO: investigate if QTabBar::setTabButton could be used to manually add
+    // a close button to the tabs that we want, and a "New..." to the chart.
     d->tabs->tabBar()->setTabsClosable(true);
-    d->tabs->addTab(new QWidget, QLatin1String("foo"));
-    d->tabs->addTab(new QWidget, QLatin1String("bar"));
+    d->tabs->addTab(d->chartView, tr("Chart"));
+
+    auto newButton = new QPushButton(tr("New"));
+    d->tabs->setCornerWidget(newButton);
+    connect(newButton, &QPushButton::clicked, [this](){
+        auto widget = new QWidget;
+        d->configurations.append(Ui::configuration());
+        d->configurations.last().setupUi(widget);
+        d->tabs->addTab(widget, tr("Configuration %1").arg(d->tabs->count()));
+        d->tabs->setCurrentWidget(widget);
+    });
 }
 
 MainWindow::~MainWindow()
