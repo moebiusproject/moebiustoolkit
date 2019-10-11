3e40c907650bb8d498a57a257303495ca31d4b4d
Alejandro Exojo <suy@badopi.org>
2019-10-11 17:58:00 +0200

    Prepare TLK test for sample files
    Files are not added to this repository because of the license, but
    prepare the scaffolding to have them in some separate location that each
    developer could provide.

--- tests/auto/tlkfile/tst_tlkfile.cpp
+++ tests/auto/tlkfile/tst_tlkfile.cpp
@@ -7,11 +7,27 @@ class tst_TlkFile: public QObject
     Q_OBJECT
 
 private slots:
+    void test_data();
     void test();
 };
 
+void tst_TlkFile::test_data()
+{
+    QTest::addColumn<QString>("fileName");
+
+    QTest::newRow("BG1 classic")
+            << QFINDTESTDATA("../../data/tlk/bg1/dialog.tlk");
+    QTest::newRow("BG1 EE")
+            << QFINDTESTDATA("../../data/tlk/bg1ee/dialog.tlk");
+}
+
 void tst_TlkFile::test()
 {
+    QFETCH(QString, fileName);
+    if (fileName.isEmpty())
+        QSKIP("File not found, skipping test.");
+    qDebug() << QTest::currentDataTag() << fileName;
+
     TlkFile file;
 }
 
