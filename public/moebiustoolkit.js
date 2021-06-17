0f586dff39bda4b56a34acb1f38046644bc40c04
Alejandro Exojo <suy@badopi.org>
2021-06-17 23:47:23 +0200

    Moebius Toolkit 0.2

---
+++ public/moebiustoolkit.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4631a67f21510f5f40dbf101b77d05701b1e77322dcbce07976d3b34bff72a96
+size 299655
---
+++ public/qtloader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:239ff19714062e8087865dde168d02104ea408cbb42a21ffdcfab5531e836335
+size 21742
